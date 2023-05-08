ad8f6b88ce9b10e91c69adf89d3205f9df4960df
Minh Nguyen <2852660+NMinhNguyen@users.noreply.github.com>
2023-05-08 01:56:16 +0100

    Create a reproduction (#1)

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,30 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+/** @jsxImportSource @emotion/react */
+import { css } from "@emotion/react";
+import styled from "@emotion/styled";
 
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+function myStyled(Component: keyof JSX.IntrinsicElements) {
+  return styled(Component)`
+    background-color: red;
+  `;
 }
 
-export default App
+function myCss(color: "red") {
+  return css`
+    background-color: ${color};
+  `;
+}
+
+const myStyles = myCss("red");
+
+const Div = myStyled("div");
+
+function App() {
+  return (
+    <>
+      <Div>one</Div>
+      <div css={myStyles}>two</div>
+    </>
+  );
+}
+
+export default App;
